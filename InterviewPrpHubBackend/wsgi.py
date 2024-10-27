ca65d9a553fd9691c56c553ac8e776a8dc05979d
Himanshu Pathak <pathakvishal132@gmail.com>
2024-10-27 19:08:33 +0530

    changed wsgi

--- InterviewPrpHubBackend/wsgi.py
+++ InterviewPrpHubBackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InterviewPrpHubBackend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "InterviewPrpHubBackend.settings")
 
 application = get_wsgi_application()
